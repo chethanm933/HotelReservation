37fbbc342817e6fef68f9fc6f0ec002c83528e67
chethanm933 <91951566+chethanm933@users.noreply.github.com>
2022-01-19 09:46:45 +0530

    Finding cheapest hotel based on weekdays and weekend rates

--- com/hotelreservation/MinCostHotel.java
+++ com/hotelreservation/MinCostHotel.java
@@ -5,6 +5,12 @@ public class MinCostHotel {
 	
 	
 	Hotels hotels = new Hotels();
+	
+	public void setHotels() {
+		hotels.setlakeWood();
+		hotels.setbridgeWood();
+		hotels.setridgewood();
+	}
 	 
 	public int minCostHotel(String input, String customerType) {
 		
